16c347d7147707f502490de4129665c2f9530f91
Alexis Gallagher <alexis@alexisgallagher.com>
2014-09-24 16:22:42 -0700

    initial commit

--- TemplateImagesBrokenDemo/ViewController.swift
+++ TemplateImagesBrokenDemo/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  // header
+  var myHeaderView:UIView?
+
+  override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    if self.myHeaderView == nil {
+      self.myHeaderView = UINib(nibName: "HeaderView", bundle: nil).instantiateWithOwner(self, options: nil).first as? UIView
+    }
+    return self.myHeaderView
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return CGFloat(150)
   }
-
+  
+  // cell
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cellReuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+    
+    
+    return cell
+  }
 
 }
 
