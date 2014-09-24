2ca02a938d1c08de78084b9801b54eb45baf3a63
Alexis Gallagher <alexis@alexisgallagher.com>
2014-09-24 15:23:08 -0700

    Initial Commit

---
+++ TemplateImagesBrokenDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TemplateImagesBrokenDemo
+//
+//  Created by Alexis Gallagher on 2014-09-24.
+//  Copyright (c) 2014 AlexisGallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
